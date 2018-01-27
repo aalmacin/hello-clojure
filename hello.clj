4f8c75e4e4d094868084d0b87cd1beda7f851a2a
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2018-01-26 22:11:55 -0500

    Added hello on multiple people passed through terminal

--- hello.clj
+++ hello.clj
@@ -1,8 +1,22 @@
 ; This is a simple hello world application
+(ns hello )
+
+(defn remove-last [items] (take (- (count items) 1) items))
+(defn get-last [items] (take 1 (reverse items)))
+(defn joiner [item] (str item ", "))
 
 (
-  defn hello
-    ([] (print "Hello World!"))
+	defn hello
+		([friends]
+			(print
+				(str
+					"Hello "
+					(reduce str (map joiner (remove-last friends)))
+					(reduce str (get-last friends))
+				)
+				; (reduce joiner ( remove-last friends ) )
+			)
+		)
 )
 
-(hello)
+(hello *command-line-args*)
